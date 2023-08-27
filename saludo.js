c408bfdbb9ebae020b38bc1335d277c0bb4af32f
Nahuel Alonso <leuhan.2010@gmail.com>
2023-08-27 20:09:08 -0300

    hola

---
+++ saludo.js
@@ -0,0 +1,8 @@
+const saludo = document.getElementById("roberto")
+const div = document.getElementById("manolo")
+saludo.addEventListener("click", ()=>{
+ alert("Hola")
+})
+div.addEventListener("click",()=>{
+    alert("Hola, Soy el Div, alias manolo.")
+})
